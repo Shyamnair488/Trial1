179e695bce12e63354ebad1bcd98a4a040ab8201
Shyam Nair <110102576+Shyamnair488@users.noreply.github.com>
2025-03-10 23:06:10 +0530

    Your new commit message

--- lib/firebase/config.ts
+++ lib/firebase/config.ts
@@ -8,14 +8,14 @@ import { Messaging } from "firebase/messaging"
 
 // Define the Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyDxXqXqXqXqXqXqXqXqXqXqXqXqXqXqXqX",
-  authDomain: "vibeconnect-7c0c3.firebaseapp.com",
-  databaseURL: "https://vibeconnect-7c0c3-default-rtdb.firebaseio.com",
-  projectId: "vibeconnect-7c0c3",
-  storageBucket: "vibeconnect-7c0c3.appspot.com",
-  messagingSenderId: "123456789012",
-  appId: "1:123456789012:web:abcdef1234567890",
-  measurementId: "G-ABCDEF1234"
+  apiKey: "AIzaSyCwt0Ng3A8KFILjAaWrXe54GIWh9cGonJg",
+  authDomain: "vibe-43e20.firebaseapp.com",
+  databaseURL: "https://vibe-43e20-default-rtdb.firebaseio.com",
+  projectId: "vibe-43e20",
+  storageBucket: "vibe-43e20.firebasestorage.app",
+  messagingSenderId: "208965831638",
+  appId: "1:208965831638:web:dc7f045d127e802cc9b277",
+  measurementId: "G-2FT28CBDE0"
 }
 
 // Initialize Firebase
